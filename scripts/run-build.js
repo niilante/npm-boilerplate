a5f6e0887462e5339f09f00a80ba23ac1573eaaa
Gregor Adams <greg@pixelass.com>
2017-02-23 10:41:41 +0100

    refactor(cleanup): removed unused dependencies
    also simplifies the build script and updates dependencies

---
+++ scripts/run-build.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const Log = require('log')
+const browserify = require('browserify')
+const watchify = require('watchify')
+const errorify = require('errorify')
+const exec = require('child_process').exec
+
+const log = new Log('info')
+const fileMap = {
+  'index.js': 'main'
+}
+
+const files = Object.keys(fileMap)
+const srcFolder = `${__dirname}/../demo`
+const buildFolder = `${__dirname}/../docs`
+
+const outFiles = files.map(file => {
+  return `${buildFolder}/${fileMap[file]}.js`
+}).join(' ')
+
+module.exports = (watch) => {
+  exec(`rm -rf ${outFiles} ${buildFolder}/index.html`, (err) => {
+    if (err) {
+      throw err
+    }
+
+    exec(`cp ${srcFolder}/index.html ${buildFolder}/index.html; cp ${srcFolder}/favico.png ${buildFolder}/favico.png`, (error) => {
+      if (error) {
+        throw error
+      }
+    })
+
+    files.forEach(file => {
+      const inFile = `${srcFolder}/${file}`
+      const outFile = `${buildFolder}/${fileMap[file]}`
+      const plugin = [errorify]
+      if (watch) {
+        plugin.push(watchify)
+      }
+      const b = browserify({
+        entries: [inFile],
+        plugin
+      })
+
+      function bundle () {
+        b.bundle().pipe(fs.createWriteStream(`${outFile}.js`))
+      }
+
+      b.on('log', message => log.info(message))
+      b.on('error', message => log.error(message))
+      if (watch) {
+        b.on('update', bundle)
+      }
+      bundle()
+    })
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * hello world function. Returns Hello + `name`
- * @param  {String} [name='World'] the name use (defaults to `'World'`)
- * @return {String}      'Hello' + `name`
+ * @param  {String} [name='World'] the `name` to use (defaults to `'World'`)
+ * @return {String} returns 'Hello' + `name`
  */
 const hello = (name = 'World') => `Hello ${name}`
 
